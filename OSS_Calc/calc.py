f14fb46d9f4f5d370ac84132c049dfe5e122a3e6
wild-boha <daidai01@naver.com>
2025-06-07 01:19:38 +0900

    [기능추가] 시속-마일 계산기 기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 class Calculator:
     def __init__(self, root):
         self.root = root
@@ -19,7 +18,7 @@ class Calculator:
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['=', '시속-마일']
         ]
 
         for row in buttons:
@@ -30,23 +29,41 @@ class Calculator:
                     frame,
                     text=char,
                     font=("Arial", 18),
-                    command=lambda ch=char: self.on_click(ch)
+                    command=lambda ch=char: self.button_click(ch)
                 )
                 btn.pack(side="left", expand=True, fill="both")
 
-    def on_click(self, char):
+    def button_click(self, char):
         if char == 'C':
             self.expression = ""
         elif char == '=':
             try:
-                self.expression = str(eval(self.expression))
+                result = str(eval(self.expression))
+                self.expression = result
             except Exception:
                 self.expression = "에러"
+        elif char == '시속-마일':
+            self.convert_kph_to_mph()
         else:
             self.expression += str(char)
 
+        self.update_entry()
+
+    def update_entry(self):
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, self.expression)
 
+    def convert_kph_to_mph(self):
+        try:
+            # 입력창의 값을 읽어서 km/h로 간주
+            kph = float(self.expression)
+            mph = kph * 0.621371
+            self.expression = f"{mph:.2f} mph"
+            self.update_entry()
+        except:
+            self.expression = "입력 오류"
+            self.update_entry()
 
-
+root = tk.Tk()
+app = Calculator(root)
+root.mainloop()
